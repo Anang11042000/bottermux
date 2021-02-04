e42221662bb2652520d90ce8084292a5c7ad2e0e
wempy24 <74690366+wempy24@users.noreply.github.com>
2021-02-04 13:34:11 +0700

    Add files via upload

---
+++ database/menu/donate.js
@@ -0,0 +1,17 @@
+const donasi = () => { 
+	return `       
+┏━━━━━━━━━━━━━━━━━━━━
+┃          𝗗𝗢𝗡𝗔𝗦𝗜  
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱ 𝗗𝗢𝗡𝗔𝗦𝗜 𝗕𝗢𝗦𝗤𝗨𝗘 ❉⊰━━✿
+┃  
+┣━⊱ *DANA*
+┣⊱ 085746897368
+┣━⊱ *PULSA*
+┣⊱ 085746897368
+┣━⊱SAWERIA
+┣⊱ https://saweria.co/wempykaguyabot
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.donasi = donasi
